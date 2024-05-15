8d2d7f5fea07cb9401a865811052f8828f562f3e
Steve Schwab <schwabs@umich.edu>
2024-05-15 12:55:49 -0700

    require server and launch it

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const server = require('./api/server');
+
+const port = 3000;
+
+server.listen(port, () => {
+    console.log(`listening to server on http://localhost:${port}`);
+})
