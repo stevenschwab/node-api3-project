ccf1583289fb4b6988927a6ded50046d6b5ef2b7
Steve Schwab <schwabs@umich.edu>
2024-05-15 14:22:05 -0700

    import global middleware to the server and the router

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const { logger } = require('../middleware/middleware');
+const usersRouter = require('./users/users-router');
 
 const server = express();
 
 server.use(express.json());
+server.use(logger)
 
-// global middlewares and the user's router need to be connected here
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
-// You will need `users-model.js` and `posts-model.js` both
-// The middleware functions also need to be required
+const { validateUserId, validateUser, validatePost } = require('../middleware/middleware');
+const Users = require('./users-model');
+const Posts = require('../posts/posts-model');
 
 const router = express.Router();
 
@@ -41,5 +42,4 @@ router.post('/:id/posts', (req, res) => {
   // and another middleware to check that the request body is valid
 });
 
-// do not forget to export the router
 module.exports = router;
