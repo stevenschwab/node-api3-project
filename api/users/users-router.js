ecd06ea840ee1fc2af71bb3622f6789f5b7c958f
Steve Schwab <schwabs@umich.edu>
2024-05-15 14:37:19 -0700

    return an array with all users for the route /api/users

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -6,8 +6,13 @@ const Posts = require('../posts/posts-model');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await Users.get();
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
@@ -40,6 +45,13 @@ router.post('/:id/posts', (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+});
+
+router.use((error, req, res, next) => { // eslint-disable-line
+  res.status(error.status || 500).json({
+    message: error.message,
+    customMessage: 'Something bad happened inside the users router',
+  });
 });
 
 module.exports = router;
