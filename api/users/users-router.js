c6e53c78f7011a9f4a0e762ee013a987a8139dd0
Steve Schwab <schwabs@umich.edu>
2024-05-15 18:22:48 -0700

    for the update route, verify the user id and check the request body is valid, then return the freshly updated user object

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -26,10 +26,12 @@ router.post('/', validateUser, (req, res, next) => {
     .catch(next)
 });
 
-router.put('/:id', validateUserId, validateUser, (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
+  Users.update(req.params.id, req.body)
+    .then(updatedUser => {
+      res.json(updatedUser);
+    })
+    .catch(next)
 });
 
 router.delete('/:id', validateUserId, (req, res, next) => {
